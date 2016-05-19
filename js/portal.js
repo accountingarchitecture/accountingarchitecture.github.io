552915581b994ee1f903f9235343ea879e6549d0
Dr. P <24814284+accountingarchitecture@users.noreply.github.com>
2016-05-19 14:07:07 -0500

    Simplified js code and fixed unhid readings error.

--- js/portal.js
+++ js/portal.js
@@ -1,5 +1,53 @@
-// Keep link to current page highlighted
+// Test for topic currency
+function isCurrentOrArchived(topic) {
+	currentDate = new Date();
+	startDate = new Date();
+	endDate = new Date();
+	startDate.setDate(currentDate.getDate() - 7);
+	endDate.setDate(currentDate.getDate() + 7);
+	if(startDate < Date.parse(topic.getElementsByClassName('date')[0].textContent) && Date.parse(topic.getElementsByClassName('date')[0].textContent) <= endDate) {
+		return 'current';
+	} else if(Date.parse(topic.getElementsByClassName('date')[0].textContent) < startDate) {
+		return 'archived';
+	}
+}
+
+// How to expand topic content
+function openTopic(topic) {
+	topic.getElementsByClassName('title')[0].className='title expand';
+	topic.getElementsByClassName('summary')[0].className='summary';
+	topic.getElementsByClassName('nav')[0].className='nav';
+}
+
+// How to hide topic content (including readings)
+function closeTopic(topic) {
+	try {
+		topic.getElementsByClassName('title')[0].className='title';
+		topic.getElementsByClassName('summary')[0].className='summary hidden';
+		topic.getElementsByClassName('nav')[0].className='nav hidden';
+		topic.getElementsByClassName('readingsbutton')[0].className='button readingsbutton';
+		topic.getElementsByClassName('readings')[0].className='readings hidden';
+	} catch(err) {
+		// Catch TypeError when
+		// (1) topics has no readings button and
+		// (2) looping to close topic (confusing...).
+	}
+}
+
+// Display topics for home page on load
+window.addEventListener('load', function(e) {
+	topics = document.getElementsByClassName('topic');
+	for(i = 0; i < topics.length; i++) {
+		if(isCurrentOrArchived(topics[i]) == 'current') {
+			topics[i].getElementsByClassName('date')[0].className='topic';
+			openTopic(topics[i]);
+		}
+	}
+});
+
+// Toggle topics and current page link on click
 document.getElementById('menu-links').addEventListener('click', function(e) {
+	// Keep link to current page highlighted
 	menuItem = document.getElementById('menu-links').getElementsByTagName('a');
 	for(i = 0; i < menuItem.length; i++) {
 		if(e.target == menuItem[i]) {
@@ -8,116 +56,52 @@ document.getElementById('menu-links').addEventListener('click', function(e) {
 			menuItem[i].className='menu-item';
 		}
 	}
-});
-
-// Load topics for home page on load
-window.addEventListener('load', function(e) {
+	// Load topics for current page
 	topics = document.getElementsByClassName('topic');
-	var topicDates = new Array();
-	currentDate = new Date();
-	startDate = new Date();
-	endDate = new Date();
-	startDate.setDate(currentDate.getDate() - 7);
-	endDate.setDate(currentDate.getDate() + 7);
 	for(i = 0; i < topics.length; i++) {
-		topicDates[i] = topics[i].getElementsByClassName('date');
-		if(startDate < Date.parse(topicDates[i][0].textContent) && Date.parse(topicDates[i][0].textContent) <= endDate) {
-			topics[i].className='topic';
-			topics[i].getElementsByClassName('title')[0].className='title expand';
-			topics[i].getElementsByClassName('summary')[0].className='summary';
-			topics[i].getElementsByClassName('nav')[0].className='nav';
-		}
-	}
-});
-
-// Load topics for current page on click
-document.getElementById('menu-links').addEventListener('click', function(e) {
-	topics = document.getElementsByClassName('topic');
-	var topicDates = new Array();
-	currentDate = new Date();
-	startDate = new Date();
-	endDate = new Date();
-	startDate.setDate(currentDate.getDate() - 7);
-	endDate.setDate(currentDate.getDate() + 7);
-	for(i = 0; i < topics.length; i++) {
-		topicDates[i] = topics[i].getElementsByClassName('date');
 		if(e.target.textContent == 'Home') {
-			if(startDate < Date.parse(topicDates[i][0].textContent) && Date.parse(topicDates[i][0].textContent) <= endDate) {
+			if(isCurrentOrArchived(topics[i]) == 'current') {
 				topics[i].className='topic';
-				topics[i].getElementsByClassName('title')[0].className='title expand';
-				topics[i].getElementsByClassName('summary')[0].className='summary';
-				topics[i].getElementsByClassName('nav')[0].className='nav';
+				openTopic(topics[i]);
 			} else {
 				topics[i].className='topic hidden';
-				topics[i].getElementsByClassName('title')[0].className='title';
-				topics[i].getElementsByClassName('summary')[0].className='summary hidden';
-				topics[i].getElementsByClassName('nav')[0].className='nav hidden';
+				closeTopic(topics[i]);
 			}
-		}
-		if(e.target.textContent == 'Archive') {
-			if(Date.parse(topicDates[i][0].textContent) < startDate) {
+		} else if(e.target.textContent == 'Archive') {
+			if(isCurrentOrArchived(topics[i]) == 'archived') {
 				topics[i].className='topic';
 			} else {
 				topics[i].className='topic hidden';
-				topics[i].getElementsByClassName('title')[0].className='title';
-				topics[i].getElementsByClassName('summary')[0].className='summary hidden';
-				topics[i].getElementsByClassName('nav')[0].className='nav hidden';
+				closeTopic(topics[i]);
 			}
 		}
 	}
 });
 
-// Show topic summary on click
+// Show topic summary or readings list on click
 document.getElementById('page').addEventListener('click', function(e) {
 	topics = document.getElementsByClassName('topic');
-	var title = new Array();
+	// Show topic summary
 	for(i = 0; i < topics.length; i++) {
-		title[i] = topics[i].getElementsByClassName('title');
-	}
-	for(i = 0; i < topics.length; i++) {
-		if(e.target == title[i][0] && title[i][0].className != 'title expand') {
-			title[i][0].className='title expand';
-			topics[i].getElementsByClassName('summary')[0].className='summary';
-			topics[i].getElementsByClassName('nav')[0].className='nav';
+		if(e.target == topics[i].getElementsByClassName('title')[0] && topics[i].getElementsByClassName('title')[0].className != 'title expand') {
+			openTopic(topics[i]);
 			for(j = 0; j < topics.length; j++) {
 				if(i != j) {
-					try {
-						title[j][0].className='title';
-						topics[j].getElementsByClassName('summary')[0].className='summary hidden';
-						topics[j].getElementsByClassName('nav')[0].className='nav hidden';
-						topics[j].getElementsByClassName('readingsbutton')[0].className='button readingsbutton';
-						topics[j].getElementsByClassName('readings')[0].className='readings hidden';
-					} catch(err) {
-					}
+					closeTopic(topics[j]);
 				}
 			}
-		} else if(e.target == title[i][0] && title[i][0].className == 'title expand') {
-			try {
-				title[i][0].className='title';
-				topics[i].getElementsByClassName('summary')[0].className='summary hidden';
-				topics[i].getElementsByClassName('nav')[0].className='nav hidden';
-				topics[i].getElementsByClassName('readingsbutton')[0].className='button readingsbutton';
-				topics[i].getElementsByClassName('readings')[0].className='readings hidden';
-			} catch(err) {
-			}
+		} else if(e.target == topics[i].getElementsByClassName('title')[0] && topics[i].getElementsByClassName('title')[0].className == 'title expand') {
+			closeTopic(topics[i]);
 		}
 	}
-});
-
-// Show readings list on click
-document.getElementById('page').addEventListener('click', function(e) {
-	topics = document.getElementsByClassName('topic');
-	var readingsButton = new Array();
+	// Show readings list on click
 	for(i = 0; i < topics.length; i++) {
-		readingsButton[i] = topics[i].getElementsByClassName('readingsbutton');
-	}
-	for(i = 0; i < topics.length; i++) {
-		if(e.target == readingsButton[i][0] && readingsButton[i][0].className != 'button readingsbutton expand') {
-			readingsButton[i][0].className='button readingsbutton expand';
+		if(e.target == topics[i].getElementsByClassName('readingsbutton')[0] && topics[i].getElementsByClassName('readingsbutton')[0].className != 'button readingsbutton expand') {
+			topics[i].getElementsByClassName('readingsbutton')[0].className='button readingsbutton expand';
 			topics[i].getElementsByClassName('readings')[0].className='readings';
-		} else if(e.target == readingsButton[i][0] && readingsButton[i][0].className == 'button readingsbutton expand') {
-				readingsButton[i][0].className='button readingsbutton';
-				topics[i].getElementsByClassName('readings')[0].className='readings hidden';
+		} else if(e.target == topics[i].getElementsByClassName('readingsbutton')[0] && topics[i].getElementsByClassName('readingsbutton')[0].className == 'button readingsbutton expand') {
+			topics[i].getElementsByClassName('readingsbutton')[0].className='button readingsbutton';
+			topics[i].getElementsByClassName('readings')[0].className='readings hidden';
 		}
 	}
 });
