3a31d3895e2fd080310e4a0c1ced9696cfdde0fd
joshua.g.coyne <joshua.g.coyne@gmail.com>
2016-06-27 09:33:35 -0400

    Added override settings for slides.

--- js/slides.js
+++ js/slides.js
@@ -1,60 +1,67 @@
 Reveal.initialize({
+	// The "normal" size of the presentation, aspect ratio will be preserved when the presentation is scaled to fit different resolutions
+		width: 1150,
+		height: 700,
+	// Factor of the display size that should remain empty around the content
+		margin: 0.1,
+	// Bounds for smallest/largest possible scale to apply to content
+		minScale: 0.2,
+		maxScale: 1.5,
 	// Display controls in the bottom right corner
-	controls: false,
+		controls: false,
 	// Display a presentation progress bar
-	progress: false,
+		progress: true,
 	// Display the page number of the current slide
-	slideNumber: true,
+		slideNumber: true,
 	// Push each slide change to the browser history
-	history: false,
+		history: false,
 	// Enable keyboard shortcuts for navigation
-	keyboard: true,
+		keyboard: true,
 	// Enable the slide overview mode
-	overview: false,
+		overview: false,
 	// Vertical centering of slides
-	center: true,
+		center: true,
 	// Enables touch navigation on devices with touch input
-	touch: true,
+		touch: true,
 	// Loop the presentation
-	loop: false,
+		loop: false,
 	// Change the presentation direction to be RTL
-	rtl: false,
+		rtl: false,
 	// Turns fragments on and off globally
-	fragments: true,
+		fragments: true,
 	// Flags if the presentation is running in an embedded mode,
 	// i.e. contained within a limited portion of the screen
-	embedded: false,
+		embedded: false,
 	// Flags if we should show a help overlay when the question mark
 	// key is pressed
-	help: true,
+		help: true,
 	// Number of milliseconds between automatically proceeding to the
 	// next slide, disabled when set to 0, this value can be overwritten
 	// by using a data-autoslide attribute on your slides
-	autoSlide: 0,
+		autoSlide: 0,
 	// Stop auto-sliding after user input
-	autoSlideStoppable: true,
+		autoSlideStoppable: true,
 	// Enable slide navigation via mouse wheel
-	mouseWheel: true,
+		mouseWheel: true,
 	// Hides the address bar on mobile devices
-	hideAddressBar: true,
+		hideAddressBar: true,
 	// Opens links in an iframe preview overlay
-	previewLinks: false,
+		previewLinks: false,
 	// Transition style
-	transition: 'slide', // none/fade/slide/convex/concave/zoom
+		transition: 'slide', // none/fade/slide/convex/concave/zoom
 	// Transition speed
-	transitionSpeed: 'default', // default/fast/slow
+		transitionSpeed: 'default', // default/fast/slow
 	// Transition style for full page slide backgrounds
-	backgroundTransition: 'default', // none/fade/slide/convex/concave/zoom
+		backgroundTransition: 'default', // none/fade/slide/convex/concave/zoom
 	// Number of slides away from the current that are visible
-	viewDistance: 3,
-
+		viewDistance: 3,
 	// Optional reveal.js plugins
-	dependencies: [
-		{ src: '../../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-		{ src: '../../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		{ src: '../../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		{ src: '../../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: '../../reveal.js/plugin/zoom-js/zoom.js', async: true },
-		{ src: '../../reveal.js/plugin/notes/notes.js', async: true }
-	]
+		dependencies: [
+			{ src: '../../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+			{ src: '../../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: '../../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: '../../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+			{ src: '../../reveal.js/plugin/zoom-js/zoom.js', async: true },
+			{ src: '../../reveal.js/plugin/notes/notes.js', async: true }
+		]
 });
