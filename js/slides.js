a114d7a2e2f6dd21535d1195bb26b51203a620c3
TheGNUdist <joshua.g.coyne@gmail.com>
2016-09-16 13:09:34 -0500

    Fixed refs to reveal.js plugins.

--- js/slides.js
+++ js/slides.js
@@ -58,12 +58,12 @@ Reveal.initialize({
 		viewDistance: 3,
 	// Optional reveal.js plugins
 		dependencies: [
-			{ src: '../../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-			{ src: '../../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-			{ src: '../../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-			{ src: '../../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-			{ src: '../../reveal.js/plugin/zoom-js/zoom.js', async: true },
-			{ src: '../../reveal.js/plugin/notes/notes.js', async: true }
+			{ src: '../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+			{ src: '../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: '../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: '../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+			{ src: '../reveal.js/plugin/zoom-js/zoom.js', async: true },
+			{ src: '../reveal.js/plugin/notes/notes.js', async: true }
 		]
 });
 
