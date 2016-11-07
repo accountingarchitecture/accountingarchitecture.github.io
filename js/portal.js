6e4dd2c81eab491a57746387a1cc7f6c5671fab1
TheGNUdist <joshua.g.coyne@gmail.com>
2016-11-07 14:30:48 -0600

    Added tutorial to Availability topic.

--- js/portal.js
+++ js/portal.js
@@ -398,6 +398,7 @@ var myModules = [
 				{"label":"Puzzle", "type":"button assignmentbutton", "href":"supplement/puzzles/gauss.html"},
 				{"label":"Puzzle", "type":"button solutionbutton", "href":"supplement/puzzles/gauss-solution.html"},
 				{"label":"Lecture", "type":"button", "href":"lectures/availability.html"},
+				{"label":"Tutorial", "type":"button", "href":"https://www.youtube.com/watch?v=fhYSE_5Dlzg"},
 				{"label":"Assignment", "type":"button", "href":"supplement/availability/assignment.html"},
 				{"label":"Readings", "type":"button readingsbutton"}
 			],
